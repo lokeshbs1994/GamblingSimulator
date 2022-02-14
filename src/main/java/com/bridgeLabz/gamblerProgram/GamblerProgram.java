fb9a6db78350b86ecbed7cf4886e16f4dcd11e9c
lokeshbs1994 <lokeshbs619@gmail.com>
2022-02-14 17:03:30 +0530

    ADDED: Total amount won or lost after 20 days

--- src/main/java/com/bridgeLabz/gamblerProgram/GamblerProgram.java
+++ src/main/java/com/bridgeLabz/gamblerProgram/GamblerProgram.java
@@ -3,18 +3,37 @@ package com.bridgeLabz.gamblerProgram;
 public class GamblerProgram {
     public static void main(String[] args) {
         System.out.println("Welcome to Gambler Program");
-        int totalAmount = 100;
+        int wins = 0;
+        int loss = 0;
         int bet = 1;
-        while(totalAmount>50 && totalAmount<150 ) {
-            double gameCheck = Math.floor(Math.random() * 10) % 2;
-            if (bet == gameCheck) {
-                System.out.println("you won the game");
-                totalAmount++;
+        int totalAmount = 0;
+        for (int day = 1; day <= 20; day++) {
+            int stake = 100;
+            while (stake > 50 && stake < 150) {
+                double gameCheck = Math.floor(Math.random() * 10) % 2;
+                if (bet == gameCheck) {
+                    //System.out.println("you won the game");
+                    stake++;
+                } else {
+                    //System.out.println("you lost the game");
+                    stake--;
+                }
+            }
+            if (stake == 50) {
+                loss++;
+                System.out.println("Day " + day + " Lost ");
             } else {
-                System.out.println("you lost the game");
-                totalAmount--;
+                wins++;
+                System.out.println("Day " + day + " Won ");
             }
         }
-        System.out.println("Total amount :" + totalAmount);
+
+        int finalAmount = wins * 50 - loss * 50;
+        if (finalAmount > 0)
+            System.out.println("After 20 days Gambler won: $ " + finalAmount);
+        else if (finalAmount < 0)
+            System.out.println("After 20 days Gambler lost: $ " + finalAmount);
+        else
+            System.out.println("No profit or loss ");
     }
 }
