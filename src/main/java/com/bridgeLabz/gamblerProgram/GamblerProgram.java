5c9853efa942a6126f4be9af2c7cfe39c68a172b
lokeshbs1994 <lokeshbs619@gmail.com>
2022-02-14 15:01:00 +0530

    ADDED: Game started

---
+++ src/main/java/com/bridgeLabz/gamblerProgram/GamblerProgram.java
@@ -0,0 +1,9 @@
+package com.bridgeLabz.gamblerProgram;
+
+public class GamblerProgram {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Gambler Program");
+        int startingTotalAmount=100;
+        int betsPerGame=1;
+    }
+}
