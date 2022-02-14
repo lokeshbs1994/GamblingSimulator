6a9af467e16e4860ff062a6c13b81e155aa44213
lokeshbs1994 <lokeshbs619@gmail.com>
2022-02-14 15:39:21 +0530

    ADDED: Check if gambler won or lost

--- src/main/java/com/bridgeLabz/gamblerProgram/GamblerProgram.java
+++ src/main/java/com/bridgeLabz/gamblerProgram/GamblerProgram.java
@@ -3,7 +3,16 @@ package com.bridgeLabz.gamblerProgram;
 public class GamblerProgram {
     public static void main(String[] args) {
         System.out.println("Welcome to Gambler Program");
-        int startingTotalAmount=100;
-        int betsPerGame=1;
+        int totalAmount = 100;
+        int bet = 1;
+        double gameCheck = Math.floor(Math.random() * 10) % 2;
+        if (bet == gameCheck) {
+            System.out.println("you won the game");
+            totalAmount++;
+        } else {
+            System.out.println("you lost the game");
+            totalAmount--;
+        }
+        System.out.println("Total amount :" + totalAmount);
     }
 }
